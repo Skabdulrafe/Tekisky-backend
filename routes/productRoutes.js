21e82956b57207d6062c2bf961290fea60051aad
shaikh rafe <skabdulrafe75@gmail.com>
2025-02-06 13:33:11 +0530

    commit

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,7 +5,7 @@ import authenticateToken from "../autharization/jwtuser.js";
 let productd=express.Router()
 productd.post('/addproduct',authenticateToken,addProduct)
 // productd.post('/upload',authenticateToken,upload)
-productd.get('/getproduct',getproduct) 
+productd.get('/getproduct',authenticateToken,getproduct) 
 productd.get('/getoneproduct/:id',authenticateToken,getOneproduct) 
 productd.put('/update/:id',authenticateToken,updateProduct)
 productd.get('/delete/:id',authenticateToken,deleteProduct)
