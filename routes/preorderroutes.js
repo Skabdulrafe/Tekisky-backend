412a494c38fdeaa2da0ada98664a904bc604c1b0
shaikh rafe <skabdulrafe75@gmail.com>
2025-02-18 16:10:02 +0530

    update

--- routes/preorderroutes.js
+++ routes/preorderroutes.js
@@ -2,7 +2,7 @@ import express from "express"
 import { getpreOrder, preorder } from "../controller/preorderController.js"
 import authenticateToken from "../autharization/jwtuser.js"
 let preorderRouter=express.Router()
-preorderRouter.post('/addpreorder',authenticateToken,preorder)
+preorderRouter.post('/addpreorder',preorder)
 preorderRouter.get('/getpreorder',authenticateToken,getpreOrder)
 export default preorderRouter
 
