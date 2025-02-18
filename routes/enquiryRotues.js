bb27ff8c4c4825ca471ff804a2b0619f88330261
shaikh rafe <skabdulrafe75@gmail.com>
2025-02-18 16:19:10 +0530

    update

--- routes/enquiryRotues.js
+++ routes/enquiryRotues.js
@@ -2,6 +2,6 @@ import express from "express"
 import { acceptenquiry,getenquiry } from "../controller/enquiryController.js"
 import authenticateToken from "../autharization/jwtuser.js"
 const enquiryRoute = express.Router()
-enquiryRoute.post('/enquiry', authenticateToken, acceptenquiry)
+enquiryRoute.post('/enquiry',acceptenquiry)
 enquiryRoute.get('/getenquiry', authenticateToken, getenquiry)
 export default enquiryRoute
