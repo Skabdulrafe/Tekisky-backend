0dcb647639519decd52e598caa1fa81abf0d81c1
shaikh rafe <skabdulrafe75@gmail.com>
2025-02-18 16:54:05 +0530

    update

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,7 +2,7 @@ import express from "express"
 import { addeorder, deleteOrder, getallorder, getOrderById1, updateOrder } from "../controller/orderController.js"
 import authenticateToken from "../autharization/jwtuser.js"
 const orderRoute = express.Router()
-orderRoute.post('/saveorder', authenticateToken,addeorder)
+orderRoute.post('/saveorder',addeorder)
 orderRoute.get('/getAllOrders', authenticateToken,getallorder)
 orderRoute.get('/getOrderById/:id', authenticateToken,getOrderById1)
 orderRoute.put('/updateOrderById/:id', authenticateToken, updateOrder)
